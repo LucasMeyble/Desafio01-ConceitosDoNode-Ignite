e2a94b0eb4a15d4a8d2e670efd1538af2db950e1
Lucas Meyble <lucasmeyble123@gmail.com>
2021-12-29 17:26:27 -0300

    fazendo rota para colocar como concluido o todo

--- src/index.js
+++ src/index.js
@@ -80,7 +80,16 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  todo.done = true;
+
+  return response.status(200).json(user.todos);
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
